bcd75b81ba7bb060434188ddd4cb6de15b449278
wangshibiao <645102170@qq.com>
2019-10-24 17:50:31 +0800

    fix: app.js的代码没有顺序执行
    因为mongo相关操作是异步的,故而没有等待数据库操作完毕,就向下执行了

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 import {config} from "config";
-let mongoClient = require("./mongo/mongoClient").mongoClient;
 
-let GameDao = require("./dao/gameDao");
-let gameDao = new GameDao();
+(async () => {
+    let mongoClient = require("./mongo/mongoClient").mongoClient;
 
-let Game = require("./model/game")
-let game = new Game({"title": "haha222"});
-gameDao.insert(game);
+    let GameDao = require("./dao/gameDao");
+    let gameDao = new GameDao();
 
-let findAll = gameDao.findAll();
-findAll.then(value => {
-    console.log("查询所有记录完成, ", value);
-})
-console.log(mongoClient)
-console.log(config);
-console.log(config.get("apiPrefix"))
+    // 插入
+    let Game = require("./model/game")
+    let game = new Game({"title": "haha222"});
+    await gameDao.insert(game);
+
+    // 查询所有记录列表
+    let gameList = await gameDao.findAll();
+    console.log("查询所有记录完成, ", gameList);
+    console.log(config);
+})();
